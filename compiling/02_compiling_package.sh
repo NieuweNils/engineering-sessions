a4e3fbf1bd2f9f35be18699838cd41c3685dfcbe
nils <nils.nieuwenkamp@eneco.com>
2023-06-02 18:21:49 +0200

    fix broken module reference

--- compiling/02_compiling_package.sh
+++ compiling/02_compiling_package.sh
@@ -3,8 +3,8 @@
 # Hello World in package com.eneco.teaching on Command Line
 echo ">> javac src/main/java/com/eneco/project/HelloWorld.java"
 echo "Compiling ..."
-javac src/main/java/com/eneco/teaching/HelloWorld.java
+javac src/main/java/com/eneco/teaching/compiling/HelloWorld.java
 echo "... Done!"
 echo ""
 echo ">> java -cp ./src/main/java/ com.eneco.project.HelloWorld"
-java -cp ./src/main/java/ com.eneco.teaching.HelloWorld
+java -cp ./src/main/java/ com.eneco.teaching.compiling.HelloWorld
--- compiling/README.md
+++ compiling/README.md
@@ -1,5 +1,6 @@
-Run the scripts one at a time, and see what files are created and what how those files can be used:
+Run the code in the scripts one line at a time, and see what files are created and what how those files can be used. 
 
+Order of the scripts:
 1. `bash 01_compiling.sh`
 2. `bash 02_compiling_package.sh`
 3. `bash 03_compiling_with_maven.sh`
