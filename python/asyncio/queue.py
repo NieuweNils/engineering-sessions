236b3f2af3d671124c77cf3139e00d395a85e29f
nils <nils.nieuwenkamp@eneco.com>
2023-10-20 15:40:18 +0200

    add names to producers and consumers in asyncio.Queue example

--- python/asyncio/queue.py
+++ python/asyncio/queue.py
@@ -1,24 +1,24 @@
 import asyncio
 
 
-async def produce(item: int, queue: asyncio.Queue) -> None:
+async def produce(name: str, item: int, queue: asyncio.Queue) -> None:
     await asyncio.sleep(0.543)
     await queue.put(item)
-    print(f"Producer added <{item}> to queue.")
+    print(f"Producer-{name} added <{item}> to queue.")
 
 
-async def consume(queue: asyncio.Queue):
+async def consume(name: str, queue: asyncio.Queue):
     while True:
         await asyncio.sleep(1.23)
         item = await queue.get()
-        print(f"Consumer got element <{item}>")
+        print(f"Consumer-{name} got element <{item}>")
         queue.task_done()
 
 
 async def job(num_messages: int):
     queue = asyncio.Queue()
-    producer_tasks = [asyncio.create_task(produce(item, queue)) for item in range(num_messages)]
-    consumer_tasks = [asyncio.create_task(consume(queue)) for _ in range(num_messages)]
+    producer_tasks = [asyncio.create_task(produce(name=str(item), item=item, queue=queue)) for item in range(num_messages)]
+    consumer_tasks = [asyncio.create_task(consume(name=str(item), queue=queue)) for item in range(num_messages)]
     await asyncio.gather(*producer_tasks)
     await queue.join()  # Implicitly awaits consumers, too
     for c in consumer_tasks:
