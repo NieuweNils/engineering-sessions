426d390d9bc85cb8f6c612976ab47ea717940283
nils <nils.nieuwenkamp@eneco.com>
2024-02-09 09:43:44 +0100

    add small fixes/alterations from during the session

--- containers/3_kubernetes/1_build.sh
+++ containers/3_kubernetes/1_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker build . -t backend-api:docker-engineering-session
+
+# # you can also skip the "deploy" step when using minikube and build the image on the minikube vm itsself
+#minikube build . -t backend-api:docker-engineering-session
