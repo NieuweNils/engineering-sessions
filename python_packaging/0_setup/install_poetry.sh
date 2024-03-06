b2ecd524a5677c7a3d15ce7417a314fd36a38d42
nils <nils.nieuwenkamp@eneco.com>
2024-03-06 21:39:58 +0100

    update install_poetry.sh

--- python_packaging/0_setup/install_poetry.sh
+++ python_packaging/0_setup/install_poetry.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 curl -sSL https://install.python-poetry.org | python3 -
+
+[[ $(type -P "poetry") ]] && echo "poetry is in PATH"  ||
+    { echo "poetry is NOT in PATH, adding it to ~/.zshrc"  && (echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc) 1>&2;}
+
 poetry completions bash >> ~/.bash_completion
 
-
-mkdir $ZSH_CUSTOM/plugins/poetry
-poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
+#sudo mkdir ~/.oh-my-zsh/custom/plugins/poetry
+#sudo poetry completions zsh > ~/.oh-my-zsh/custom/plugins/poetry/_poetry
+# also add the poetry plugin to zsh
